424c73b44f87c0ec8e518e5682cdef6a2ab9472b
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-02-16 11:34:02 -0800

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+
+/**
+ * main - Enty point
+ *
+ * Return: 1 (error)
+ */
+int main(void)
+{
+	char msg[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, msg, sizeof(msg) - 1);
+	return (1);
+}
