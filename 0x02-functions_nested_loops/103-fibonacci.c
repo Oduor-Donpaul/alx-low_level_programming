262e35f27345f955be1c9f6ff5eedc37ca8947a8
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-02-21 18:12:27 -0800

    commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num1 = 1;
+	int num2 = 2;
+	int next_num = 0;
+	int sum = 2; // Start with the sum of the first even number (2)
+
+	while (next_num <= 4000000)
+	{
+		next_num = num1 + num2;
+		num1 = num2;
+		num2 = next_num;
+		if (next_num % 2 == 0)
+		{
+		sum += next_num;
+		}
+	}
+	printf("%d", sum);
+}
