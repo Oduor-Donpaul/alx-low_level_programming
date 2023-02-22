6911e29b989f167c96bc5c0458e0f3ff39d4f47d
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-02-21 17:37:16 -0800

    commit

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry 
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int num = 1, num_one = 1, num_two = 2, num_next;
+
+	while (num <= 50)
+	{
+		if (num = 1)
+		{
+			printf("%d", num_one);
+		} else if ( num = 2)
+		{
+			printf(", %d", num_two);
+		} else
+		{
+			num_next = num_one + num_two;
+
+			 printf(", %d", num_next);
+		}
+		num++;
+	}
+	printf("\n");
+}
