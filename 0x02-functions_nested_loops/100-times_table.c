b9688cefc7b873267e48c6e1780da1407474c002
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-02-21 16:52:24 -0800

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,11 +13,11 @@ void print_times_table(int n)
 		return;
 	for (i = 0; i <= n; i++)
 	{
-		for (j = 0; j <=n; j++)
+		for (j = 0; j <= n; j++)
 		{
 			int num = i * j;
 
-			if (j==0)
+			if ( j==0 )
 				_putchar(num + '0');
 			else
 			{
