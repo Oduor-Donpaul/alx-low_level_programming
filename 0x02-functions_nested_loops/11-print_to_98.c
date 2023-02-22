76cd8d080eed77a4ad126dfe4fbbef2360c16a3e
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-02-21 16:06:20 -0800

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			if (n <98)
+			if (n < 98)
 			{
 				printf("%d, ", n);
 			} else
@@ -21,7 +21,7 @@ void print_to_98(int n)
 			}
 		}
 
-	}else if (n == 98)
+	} else if (n == 98)
 	{
 		printf("%d\n", n);
 	} else
