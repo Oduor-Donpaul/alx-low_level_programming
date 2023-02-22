7f53cc8fe56656a4964015030d8ce6d245f3e693
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-02-21 16:47:23 -0800

    commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * print_times_table - prints n times table
+ * @n: int
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n < 0 || n > 15)
+		return;
+	for (i = 0; i <= n; i++)
+	{
+		for (j = 0; j <=n; j++)
+		{
+			int num = i * j;
+
+			if (j==0)
+				_putchar(num + '0');
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				if (num < 10)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(num + '0');
+				}
+				else if (num < 100)
+				{
+					_putchar(' ');
+					_putchar((num / 10) + '0');
+					_putchar((num % 10) + '0');
+				}
+				else
+				{
+					_putchar((num / 100) + '0');
+					_putchar((num / 10 % 10) + '0');
+					_putchar((num % 10) + '0');
+				}
+			}
+		}
+		_putchar('\n');
+	}
+}
