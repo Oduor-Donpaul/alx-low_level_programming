35bf2ee0415e758067ebb2f62a9bbd8d6b3a3871
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-02-23 09:55:02 -0800

    commit

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,98 @@
+#include <unistd.h>
+/**
+ * _putchar - input (char)
+ *
+ * Return: int 
+ */
+int _putchar(char c);
+
+/**
+ * print_alphabet - input (void)
+ *
+ * Return: void
+ */
+void print_alphabet(void);
+
+/**
+ * print_alphabet_x10 - input (void)
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void);
+
+/**
+ * _islower - input (int)
+ *
+ * Return: int
+ */
+int _islower(int c);
+
+/**
+ * _isalpha - input (int)
+ *
+ * Return: int
+ */
+int _isalpha(int c);
+
+/**
+ * print_sign - input (int)
+ *
+ * Return: int
+ */
+int print_sign(int n);
+
+/**
+ * -abs - input (int)
+ *
+ *  Return: int
+ */
+int _abs(int);
+
+/**
+ * print_last_digit - input (int)
+ *
+ * Return: int
+ */
+int print_last_digit(int);
+
+/**
+ * jack_bauer - input (void)
+ *
+ * Return: void
+ */
+void jack_bauer(void);
+
+/**
+ * times_table - input (void)
+ *
+ * Return: void
+ */
+void times_table(void);
+
+/**
+ * add - input (int, int)
+ *
+ * Return: int
+ */
+int add(int, int);
+
+/**
+ * print-to_98 - input (int)
+ *
+ * Return: void
+ */
+void print_to_98(int n);
+
+/**
+ * print_times - input (int)
+ *
+ * Return: void
+ */
+void print_times_table(int n);
+
+/**
+ * _isupper - input(int)
+ *
+ * Return: int
+ */
+int _isupper(int c);
