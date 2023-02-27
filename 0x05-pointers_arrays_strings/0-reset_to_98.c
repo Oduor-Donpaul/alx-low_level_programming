5176aae68f19cd44e866e308864c2586e9dedff6
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-02-27 09:06:20 -0800

    commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * reset_to_98 - updates value to 98
+ * @n: int
+ *
+ * Return: void
+ */
+void reset_t_98(int *n)
+{
+	*n = 98;
+
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * reset_to_98 - updates the value of the input to 98
+ * @n: int
+ *
+ * Return: void
+ */
+void reset_to_98(int *n);
+#endif
