c03a6643cd1ad6c57645b9cb09b190341220becc
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-03-02 11:01:56 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * leet - encodes string into 1337
+ * @str: string to be encoded
+ *
+ * Return: char
+ */
+char *leet(char *str)
+{
+	int i = 0, j = 0;
+	int len = 0;
+
+	while (str[i])
+		i++;
+
+	while (str[i])
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+		{
+			str[i] = '4';
+		} else if (str[i] == 'e' || str[i] == 'E')
+		{
+			str[i] = '3';
+		} else if (str[i] == 'o' || str[i] == 'O')
+		{
+			str[i] = '0';
+		} else if (str[i] == 't' || str[i] == 'T')
+		{
+			str[i] = '7';
+		} else if (str[i] == 'l' || str[i] == 'L')
+		{
+			str[i] = '1';
+		}
+		i++;
+	}
+	return (str);
+}
