2cafb89a6d8093dd09cd951de3687b069245b14a
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-03-02 10:33:57 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all wods of a string
+ * @str: char
+ *
+ * Return: char
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		while (!(str[i] >= 'a' && str[i] <= 'z'))
+			i++;
+		if (str[i - 1] == ' ' ||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			i == 0)
+			str[i] -= 32;
+		i++;
+	}
+	return (str);
+}
