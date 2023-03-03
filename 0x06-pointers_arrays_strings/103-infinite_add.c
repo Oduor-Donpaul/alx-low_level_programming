37a2bc7c675cd73c98d4d5c0e19289554cb6e070
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-03-02 18:00:48 -0800

    commit

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * infinite_add - adds two integers
+ * @n1: number to be added
+ * @n2: number to be added
+ * @r: buffer that will store result
+ * @size_r: buffer size
+ *
+ * Return: char
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+{
+	r = n1 + n2;
+
+	return (r = size_r);
+}
