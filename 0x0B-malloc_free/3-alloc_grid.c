9acebc6f719f0710431962a2c6815fa4d38ebe4d
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-03-14 05:16:48 -0700

    commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -30,12 +30,13 @@ int **alloc_grid(int width, int height)
 			for (j = 0; j < i; j++)
 				free(ptr[i]);
 			free(ptr);
+			return (NULL);
 		}
 		for (j = 0; j < i; j++)
 			ptr[i][j] = 0;
 	}
-	return (ptr);
 	for (i = 0; i < height; i++)
 		free(ptr[i]);
 	free(ptr);
+	return (ptr);
 }
