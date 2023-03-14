a2681661ea155683b6d66312ff0fc02709951f55
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-03-14 00:16:19 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,11 @@ char *str_concat(char *s1, char *s2)
 {
 	unsigned int len1, len2, i = 0, j = 0;
 	char *ptr;
-
+	
+	if (s1 == NULL)
+		s1[] = "";
+	if (s2 == NULL)
+		S2[] = "";
 	while (s1[i])
 	{
 		i++;
