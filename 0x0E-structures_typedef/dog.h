b97865f355aa7a4f4fd43bbe12b78e271c429bf1
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-03-20 12:53:39 -0700

    initializing 0x0E-structures_typedef directory with README.md and dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of a dog
+ * @name: pointer to name variable of type char
+ * @age: pointer to age variable of type float
+ * @owner: pointer to owner variable of type char
+ *
+ * Description: struct dog contains elements
+ * name age owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
