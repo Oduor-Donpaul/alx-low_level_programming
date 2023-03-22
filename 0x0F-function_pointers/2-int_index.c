0ae42b90de88886305e358418a4e5d4195c35d92
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-03-22 11:54:18 -0700

    updating 2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,23 +6,21 @@
  * @array: array
  *
  * Return: index of the first element for which cmp
- * 	function does not return 0
- * 	if no element matches, return -1
- * 	if size <= 0, return -1
+ *	function does not return 0
+ *	if no element matches, return -1
+ *	if size <= 0, return -1
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
-	int p;
 
 	if (array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
-		p = cmp(array[i]);
+		if (cmp(array[i]) != 0)
+			return (i);
 	}
-	if (p != 0)
-		return (array[i]);
 	return (-1);
 }
