03ef3553d2986d5563934d805b75fb7b924a38a9
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-03-23 15:40:40 -0700

    updating 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		p = va_arg(ap, char*);
 
 		if (p == NULL)
-			printf("(nil)";
+			printf("(nil)");
 
 		if (separator != NULL && i != n-1)
 			printf("%s%s", p, separator);
