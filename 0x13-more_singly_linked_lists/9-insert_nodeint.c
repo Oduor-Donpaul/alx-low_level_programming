429655fddda64dc1b3b5d42350f0998a5f93a69b
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-04-04 10:31:42 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -39,10 +39,5 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new_node->next = temp->next;
 	temp->next = new_node;
 
-	if (new_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
 	return (new_node);
 }
