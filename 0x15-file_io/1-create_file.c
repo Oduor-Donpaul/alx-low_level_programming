d9a60694701508ab0820ab4aa065042472185ae9
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-04-09 13:25:19 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,8 +20,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-
-	write(fd, text_content, strlen(text_content));
+	
+	if (text_content != NULL)
+		write(fd, text_content, strlen(text_content));
 
 	close(fd);
 
