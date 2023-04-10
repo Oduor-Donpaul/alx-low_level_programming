bc80ed2d3a54f715581edaed6f7b49a801412a7a
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-04-10 00:25:44 -0700

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -20,10 +20,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content == NULL)
-		return (-1);
+	if (text_content != NULL)
 
-	write(fd, text_content, strlen(text_content));
+		write(fd, text_content, strlen(text_content));
 
 	close(fd);
 
