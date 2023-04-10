d76dff50fd50a35f925ea8678c99a3b7d55d62bb
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-04-10 14:44:47 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,40 +6,47 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+/**
+ * read_textfile - reads textfile
+ * @filename: file name
+ * @letters: letters
+ * Return: int
+ */
+
 size_t read_textfile(const char *filename, size_t letters)
 {
-   int fd;
-   int n;
-   char *buffer;
+	int fd;
+	int n;
+	char *buffer;
 
-   if (filename == NULL)
-           return (0);
+	if (filename == NULL)
+		return (0);
 
-   fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
-   if (fd == -1)
-           return (0);
+	if (fd == -1)
+		return (0);
 
-   buffer = malloc(sizeof(char) * (letters + 1));
+	buffer = malloc(sizeof(char) * (letters + 1));
 
-   if (buffer == NULL)
-   {
-           close(fd);
-           return (0);
-   }
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
 
-   n = read(fd, buffer, letters);
+	n = read(fd, buffer, letters);
 
-   if (n == -1)
-   {
-           free(buffer);
-           close(fd);
-           return (0);
-   }
+	if (n == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
 
-   write(STDOUT_FILENO, buffer, n);
+	write(STDOUT_FILENO, buffer, n);
 
-   free(buffer);
-   close(fd);
-   return (n);
+	free(buffer);
+	close(fd);
+	return (n);
 }
