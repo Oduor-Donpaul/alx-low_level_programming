537679ff32d1545a5988d0f7dad5f083b855758a
Oduor-Donpaul <donpaul.oduordonpaul@gmail.com>
2023-04-10 04:45:43 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -5,6 +5,8 @@
 
 /**
  * main - copies content from file 1 to file 2
+ * @ac: argument count
+ * @av: ARGUMENT VECTOR
  * Return: nothing
  */
 int main(int ac, char **av)
@@ -19,25 +21,22 @@ int main(int ac, char **av)
 		exit(97);
 	}
 	fd = open(av[1], O_RDONLY);
-
 	if (fd == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-
-
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		close(fd);
 		exit(99);
 	}
 	while ((buf_read = read(fd, buf, 1024)) > 0)
 	{
 		buf_write = write(fd_to, buf, buf_read);
-		if (buf_write != buf_read)
+		if (buf_write == -1 || buf_wrte != buf_read)
 		{
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			close(fd);
@@ -45,19 +44,11 @@ int main(int ac, char **av)
 			exit(99);
 		}
 	}
-
-	fd_close = close(fd);
-	fd_close_1 = close(fd_to);
-
-	if (fd_close == -1)
+	close(fd);
+	close(fd_to);
+	if (close(fd) == -1 || close(fd_to) == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d", fd);
-		exit(100);
-	}
-
-	if (fd_close_1 == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d", fd_to);
 		exit(100);
 	}
 	return (0);
