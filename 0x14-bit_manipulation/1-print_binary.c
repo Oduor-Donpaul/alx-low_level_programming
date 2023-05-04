0d9a12720c7b6a123de9ba14fe52a58dcff220ba
Donpaul Oduor <donpaul.oduordonpaul@gmail.com>
2023-05-04 06:21:08 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -18,20 +18,3 @@ void print_binary(unsigned long int n)
 
 	write(1, &c, 1);
 }
-
-
-
-int main(void)
-{
-    print_binary(0);
-    printf("\n");
-    print_binary(1);
-    printf("\n");
-    print_binary(98);
-    printf("\n");
-    print_binary(1024);
-    printf("\n");
-    print_binary((1 << 10) + 1);
-    printf("\n");
-    return (0);
-}
