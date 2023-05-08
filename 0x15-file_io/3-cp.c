e2645057f7d8caaf1a8e4e7324bfe3bdddf43590
Donpaul Oduor <donpaul.oduordonpaul@gmail.com>
2023-05-08 11:09:41 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,12 +11,12 @@ void _error(int fd, int fd_2, char *av[])
 {
 	if (fd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from the file %s\n", av[1]);
+		dprintf(STDERR_FILENO, "Error: can't read from %s\n", av[1]);
 		exit(98);
 	}
 	if (fd_2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: can't read from the file %s\n", av[2]);
+		dprintf(STDERR_FILENO, "Error: can't write to %s\n", av[2]);
 		exit(99);
 	}
 }
@@ -32,7 +32,7 @@ int main(int ac, char *av[])
 {
 	char buffer[1024];
 	int fd, fd_2;
-	ssize_t no_write, no_read = 1024;
+	ssize_t no_read = 1024;
 
 	if (ac != 3)
 	{
@@ -42,18 +42,21 @@ int main(int ac, char *av[])
 	fd = open(av[1], O_RDONLY);
 	fd_2 = open(av[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
 	_error(fd, fd_2, av);
-	while (no_read == 1024)
+	while ((no_read == read(fd, buffer, 1024)) > 0)
 	{
-		no_read = read(fd, buffer, 1024);
-		if (no_read == -1)
-			_error(-1, 0, av);
-		no_write = write(fd_2, buffer, no_read);
-		if (no_write == -1)
+		if (write(fd_2, buffer, no_read) != no_read)
 			_error(0, -1, av);
 	}
-	if (close(fd) == -1 || close(fd_2) == -1)
+	if (no_read < 0)
+		_error(-1, 0, av);
+	if (close(fd) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d", fd);
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+	if (close(fd_2) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_2);
 		exit(100);
 	}
 	return (0);
