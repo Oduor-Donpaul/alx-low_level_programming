744853b291546ef03c1fc0c5e3f5c19cab12cc38
Donpaul Oduor <donpaul.oduordonpaul@gmail.com>
2023-07-07 19:07:56 +0300

    correction 3-hash_table_set

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = key_index((const unsigned char *)key, ht->size);
 
-	hash_node_t *new_node = (hash_node_tmalloc(sizeof(hash_node_t));
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
 
 	if (!new_node)
 	{
