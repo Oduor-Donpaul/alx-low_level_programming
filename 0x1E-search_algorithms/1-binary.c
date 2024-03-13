48bc7c1e6b8b47051eadbb9ef9a0fbcfe5b87b3c
Donpaul <123797405+Oduor-Donpaul@users.noreply.github.com>
2024-03-13 20:55:28 +0300

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,38 +1,49 @@
 #include "search_algos.h"
-
+#include <stdio.h>
 /**
- * binary_search - uses binary seach to search an array
- * @array: pointer to first element of ana array
- * @value: value to be searched
- * @size: number of elements in the array
- * Return: index of the value or -1 if array is NULL
+ * print_arr - Print an array
+ * @arr: The array to print
+ * @start: The begining of the array to print
+ * @end: The end of the array to print
+ */
+void print_arr(int *arr, int start, int end)
+{
+	int i;
+
+	printf("Searching in array:");
+	for (i = start; i < end; i++)
+		printf(" %d,", arr[i]);
+	printf(" %d\n", arr[end]);
+}
+/**
+ * binary_search - Make a binary search in an ordered list
+ * @array: The list to search
+ * @size: The size of the list
+ * @value: The value to search for
+ *
+ * Return: The index of the value you're looking for, or -1
+ *	   If no match was found, or an empty array was given
  */
 int binary_search(int *array, size_t size, int value)
 {
-	int left, right, mid;
+	size_t start = 0, end = size - 1, temp;
 
 	if (array == NULL)
+		return (-1);
+	for (start = 0, end = size - 1; start <= end; )
 	{
-		return -1;
-	}
-
-	left = 0;
-	right = size - 1;
-
-	while (left <= right)
-	{
-		mid = left + (right - left) / 2;
-		if (array[mid] == value)
+		print_arr(array, start, end);
+		temp = (start + (end - start) / 2);
+		if (value > array[temp])
 		{
-			return mid;
+			start = temp + 1;
 		}
-		if (array[mid] < value)
+		else
 		{
-			left = mid + 1;
-		} else
-		{
-			right = mid - 1;
+			end = temp - 1;
 		}
+		if (array[temp] == value)
+			return (temp);
 	}
-	return -1;
+	return (-1);
 }
